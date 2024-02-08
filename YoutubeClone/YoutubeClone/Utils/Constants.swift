2f2c8adb7f65dbef88e6fcec499b91c13a01906c
inmiti <inmabauvel@gmail.com>
2024-02-08 19:58:12 +0100

    Elimino constantes

--- YoutubeClone/YoutubeClone/Utils/Constants.swift
+++ YoutubeClone/YoutubeClone/Utils/Constants.swift
@@ -1,13 +1,7 @@
 //
-//  Constants.swift
+//  Constants1.swift
 //  YoutubeClone
 //
-//  Created by ibautista on 1/2/24.
+//  Created by ibautista on 8/2/24.
 //
 
-import Foundation
-
-struct Constants {
-    static let apikey = "AIzaSyDj7OnEdhsLxYpnqzBBCPf35yYJwSu3jec"
-    static let channelId = "UC_I_lCplbwIf60iUgsTE79g"
-}
---
+++ YoutubeClone/YoutubeClone/Utils/Constants_modificar.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  YoutubeClone
+//
+//  Created by ibautista on 1/2/24.
+//
+
+import Foundation
+
+struct ConstantsModificar{
+    static let apikey = "Pon aquí tu apikey"
+    static let channelId = "Pon aquí el channelId"
+}
